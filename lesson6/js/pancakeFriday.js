ad129d4a8352cbc632f3d9d9016cd2f06e6a633a
ankefisher <fis19013@byui.edu>
2021-02-10 14:13:55 -0600

    lesson6

---
+++ lesson6/js/pancakeFriday.js
@@ -0,0 +1,4 @@
+if(dayName == 'Friday') {
+    document.getElementById("pancakes").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
+    document.getElementById("pancakes").classList.add("show");
+ }
