076881f061da15e4ff789fe28028542d5024c56d
ankefisher <fis19013@byui.edu>
2021-02-12 12:46:54 -0600

    lesson5

---
+++ lesson6/js/windchill.js
@@ -0,0 +1,13 @@
+function (){
+    const hightemp = 45;
+    const windspeed = 5;
+    let windchill = 35.74 + (0.6215 * hightemp) - (35.75 * (windspeed ** .16)) + (0.4275 * hightemp * (windspeed ** .16));
+    
+     
+    if (hightemp <= 50 && windspeed > 3) {
+       windchill = Math.round(windchill) + "&#8457";
+    } else {
+       windchill = "N/A";
+    }
+    document.getElementById('windchill').innerHTML = windchill;
+  };
