01ab0143fccef6a0aa3eff6973c5cead73a1032c
ankefisher <fis19013@byui.edu>
2021-02-26 15:44:54 -0600

    lesson8

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,15 +1,15 @@
 {
-    const hightemp = 49;
-    const windspeed = 5;
-    let windchill = 35.74 + (0.6215 * hightemp) - (35.75 * (windspeed ** .16)) + (0.4275 * hightemp * (windspeed ** .16));
+   const hightemp = document.getElementById('hightemp').innerHTML;
+   const windspeed = document.getElementById('windspeed').innerHTML;
+   let windchill = 35.74 + (0.6215 * hightemp) - (35.75 * (windspeed ** .16)) + (0.4275 * hightemp * (windspeed ** .16));
+   
     
-     
-    if (hightemp <= 50 && windspeed > 3) {
-       windchill = Math.round(windchill);
-    } else {
-       windchill = "N/A";
-    }
-    document.getElementById('windchill').innerHTML = windchill;
+   if (hightemp <= 50 && windspeed > 3) {
+      windchill = Math.round(windchill) + "&#8457";
+   } else {
+      windchill = "N/A";
+   }
+   document.getElementById('windchill').innerHTML = windchill;
   };
 
  // used work:
@@ -18,5 +18,7 @@
  // https://www.sitepoint.com/community/t/functions-calling-on-other-functions/280970
  // my CIT 160 course material
  // help from the discussion board
+
+
 
 
